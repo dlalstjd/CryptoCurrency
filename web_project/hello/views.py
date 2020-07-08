65f742b77f5ad39a3724a9d72367205ba9a7856b
dlalstjd <alstjd8252@naver.com>
2020-07-08 14:32:54 +0900

    get log from db

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 import re
 from datetime import datetime
 
+from hello.models import LogMessage
+from hello.forms import LogMessageForm
+from django.shortcuts import redirect
+
+from django.views.generic import ListView
+
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -45,11 +51,31 @@ def self_introduction(request):
         'hello/self_introduction.html'
     )
 
-def home(request):
-    return render(request, "hello/home.html")
+
+#def home(request):
+#    return render(request, "hello/home.html")
+
+class HomeListView(ListView):
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def about(request):
     return render(request, "hello/about.html")
 
 def contact(request):
     return render(request, "hello/contact.html")
+    
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form":form})

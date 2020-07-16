c4146e706b5563aeb880cea74fda1968854e50b4
dlalstjd <alstjd8252@naver.com>
2020-07-16 15:44:54 +0900

    transaction

--- web3js/security_test.js
+++ web3js/security_test.js
@@ -5,7 +5,7 @@ import _Tx from 'ethereumjs-tx'
 //console.log(Web3)
 
 // using ropsten as endpoints
-var url = 'https://ropsten.infura.io/v3/375a84d45ba0456a8d39a32cce31471c'
+const url = 'https://ropsten.infura.io/v3/375a84d45ba0456a8d39a32cce31471c'
 // connect to any peer; using infura here
 const web3 = new Web3(new Web3.providers.HttpProvider(url));
 
@@ -22,21 +22,18 @@ const account = web3.eth.accounts.privateKeyToAccount(web3.utils.toHex(privateKe
 
 //const Wallet = web3.eth.accounts.wallet.create();
 //Wallet.add(account)
-//console.log(Wallet)
 
-//console.log(_Tx)
 const { Transaction : Tx } = _Tx
 
 const rawTransaction = {
-    from: "0x4f8D6BE7DD4FebDbf8AC3a7c582d5897C1422B32",
-    to: "0x743376fd2a693723A60942D0b4B2F1765ea1Dbb0",
-    //value: web3.utils.toHex(web3.utils.toWei("0.01"))
-    value: '0x2386f26fc1000',
-    gas: web3.utils.toHex(web3.utils.toWei("10", "Kwei")),
-    // "chainId": 8888
-  };
+  from: "0x4f8D6BE7DD4FebDbf8AC3a7c582d5897C1422B32",
+  to: "0x743376fd2a693723A60942D0b4B2F1765ea1Dbb0",
+  value: web3.utils.toHex(web3.utils.toWei('0.001', 'ether')),
+  gasLimit: web3.utils.toHex(21000),
+  gasPrice: web3.utils.toHex(50e9)
+};
 
-var tx = new Tx(rawTransaction, { chain: 'ropsten', hardfork: 'petersburg' })
+var tx = new Tx(rawTransaction, { chain: 'ropsten', hardfork: 'petersburg' }, )
 tx.sign(privateKey)
 
 const serializedTx = tx.serialize()

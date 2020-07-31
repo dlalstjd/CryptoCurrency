ff4f0f3cb1e680672e12fb3a20837028b2c77147
dlalstjd <alstjd8252@naver.com>
2020-07-31 18:27:35 +0900

    test for tss

--- web3js/signature_test.js
+++ web3js/signature_test.js
@@ -4,6 +4,9 @@ import keccak from 'js-sha3'
 import secp256k1 from 'secp256k1'
 //import rlphash from 'ethereumjs-util'
 import util from 'ethereumjs-util'
+import txDecoder from 'ethereum-tx-decoder'
+//var txDecoder = require('ethereum-tx-decoder');
+
 //import * as SSS from './SSS.js'
 
 const url = 'https://ropsten.infura.io/v3/375a84d45ba0456a8d39a32cce31471c'
@@ -13,27 +16,42 @@ const{ keccak256 } = keccak
 const{ ecsign } = util
 
 //const from_address = "0x4f8d6be7dd4febdbf8ac3a7c582d5897c1422b32"
-const from_address = "99f039bffe27d2a868f46c49a5889920f68eabfe"
+const from_address = "0x99f039bffe27d2a868f46c49a5889920f68eabfe"
 const to_address = "0x743376fd2a693723A60942D0b4B2F1765ea1Dbb0"
 
 const rawTransaction = {
     from: from_address,
     to: to_address,
-    nonce: web3.utils.toHex(6),
+    nonce: web3.utils.toHex(0),
     value: web3.utils.toHex(web3.utils.toWei('0.001', 'ether')),
     gasLimit: web3.utils.toHex(21000),
-    gasPrice: web3.utils.toHex(50e9)
+    gasPrice: web3.utils.toHex(50e9),
 };
 
+const msg = [249,85,74,2,162,31,189,136,48,112,14,229,173,106,49,161,235,40,95,214,70,29,254,155,217,50,65,137,44,197,205,44]
+const hex = Buffer.from(msg).toString('hex')
+console.log(hex)
+
 var tx = new Tx(rawTransaction, { chain: 'ropsten', hardfork: 'petersburg' }, )
-console.log(tx.raw)
-const msgHash = keccak256(util.rlphash(tx.raw))
 
-const uintMsg = Uint8Array.from(Buffer.from(msgHash, 'hex'))        
-const privateKey = Buffer.from("9718f5e8049711bdf4f4f2ca7dc98d3b6ddf9e4bfe4aaa2f5c97757380738928", "hex")
+const r = "85154094909487471260476513072605173124555759920348708084928921321576147112055"
+const s = "36174566012658177741771168398925546663644885796058154329750432761580780913620"
 
-const Sig = ecsign(uintMsg, privateKey)
-console.log(Sig)
-//const backToHex = Buffer.from(Sig.signature).toString('hex')        
-//console.log(backToHex)
+tx.r = web3.utils.toHex(r)
+tx.s = web3.utils.toHex(s)
+tx.v = web3.utils.toHex(27)
+
+// console.log(tx)
+
+const serializedTx = tx.serialize()
+// console.log(serializedTx)
+
+const raw = '0x' + serializedTx.toString('hex')
+console.log(raw);
+
+console.log(txDecoder.decodeTx(raw))
+
+//web3.eth.sendSignedTransaction( raw, (err, txHash) => {
+//    console.log('err:', err, 'txHash: ', txHash)
+//})
 
